10cd7bfae47e9292c84c6423d0e1b81cff679e21
William Russell <williamrussell@Williams-MBP.lan>
2023-02-01 00:07:35 -0500

    commit tree d80c68be661b631de297d0b06899a4d23adae2fc

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if(number <= 400){
+    return 'This one is on me!'
+  } else if(number === 401 && number > 2000){
+    return 'That will be twenty dollars.'
+  }
 }
+  // Write your code here!
+
 
 function ternaryCheckCity(){
   // Write your code here!
